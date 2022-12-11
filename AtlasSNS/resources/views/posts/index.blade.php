e0429455178abdfa8fdec59ff50a09a8c24cdce7
田島　隆太 <>
2022-12-11 12:10:18 +0900

    投稿フォーム箱作成

--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -4,4 +4,12 @@
 <h2>機能を実装していきましょう。</h2>
 
 
+<!-- <p class="pull-right"><a class="" href=""><img src="images/post.png"></a></p> -->
+<h2 class="page-header"><img src="images/icon1.png"></h2>
+{!! Form::open(['url' => '']) !!}
+    <div class="form-group">
+      {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください']) !!}
+    </div>
+    <button type="submit" class="btn btn-success pull-right"><img src="images/post.png"></button>
+{!! Form::close() !!}
 @endsection
