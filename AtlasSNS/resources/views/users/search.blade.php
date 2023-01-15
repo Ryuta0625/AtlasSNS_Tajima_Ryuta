6178731ef16bc627a98c43f3e717b3fc528bd2f2
田島　隆太 <>
2023-01-15 17:16:19 +0900

    検索フォーム設置

--- AtlasSNS/resources/views/users/search.blade.php
+++ AtlasSNS/resources/views/users/search.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.login')
 
 @section('content')
-
-
+    <div class="form-group">
+      {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => 'ユーザー名']) !!}
+    </div>
+<button type="submit" class="btn btn-success pull-right"><img src=""></button>
 
 @endsection
