476e9efc69ca9c2c1a99b6aec8856f283444741d
田島　隆太 <>
2023-02-25 12:19:05 +0900

    投稿ボタン変更 編集、削除ボタン追加

--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -8,14 +8,16 @@
 {!! Form::open(['url' => '/create']) !!}
     <div class="form-group">
       {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください']) !!}
+      <button type="submit"><img src="images/post.png" alt="button" width="80" height="80"></button>
     </div>
-    <button type="submit" class="btn btn-success pull-right"><img src="images/post.png"></button>
 {!! Form::close() !!}
     <div>
       @foreach ($list as $list)
       <tr>
         <td>{{ $list->post }}</td>
         <td>{{ $list->created_at }}</td>
+        <td><a class="btn btn-primary" href="/post/{{$list->id}}/update-form">更新</a></td>
+        <td><a class="btn btn-danger" href="/post/{{$list->id}}/delete" onclick="return confirm('こちらの投稿を削除してもよろしいでしょうか？')">削除</a></td>
       </tr>
       @endforeach
     </div>
